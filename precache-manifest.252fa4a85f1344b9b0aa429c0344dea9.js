8fedf39553c5185e7ad3f693861e22f75bf0e798
EpicNicks <epicnicks2.0@gmail.com>
2019-08-15 00:21:37 -0400

    Updates

---
+++ precache-manifest.252fa4a85f1344b9b0aa429c0344dea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f17c78f945d237020241fe2418957b",
+    "url": "/nicholas-cerisano-portfolio/index.html"
+  },
+  {
+    "revision": "aedfb60931285e26a8bb",
+    "url": "/nicholas-cerisano-portfolio/static/css/main.19393e92.chunk.css"
+  },
+  {
+    "revision": "11304c4e6113e5ffbfc8",
+    "url": "/nicholas-cerisano-portfolio/static/js/2.581b28c2.chunk.js"
+  },
+  {
+    "revision": "aedfb60931285e26a8bb",
+    "url": "/nicholas-cerisano-portfolio/static/js/main.102d8655.chunk.js"
+  },
+  {
+    "revision": "f0d4829072d2eb1e75b3",
+    "url": "/nicholas-cerisano-portfolio/static/js/runtime~main.56d20093.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/nicholas-cerisano-portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
