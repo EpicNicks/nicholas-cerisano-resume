53dade985b47bf7e8bea4a403860a84ee626ad14
EpicNicks <epicnicks2.0@gmail.com>
2019-08-31 22:21:59 -0400

    add static

---
+++ build/precache-manifest.043ba3d35cfbfa894af6f7b318793233.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ffb577a4649d5b8d0a83db953333a3e",
+    "url": "/nicholas-cerisano-resume/index.html"
+  },
+  {
+    "revision": "7ff116c93424d91c22e8",
+    "url": "/nicholas-cerisano-resume/static/css/main.b8edc1bf.chunk.css"
+  },
+  {
+    "revision": "7e8d5c828682567c8ba9",
+    "url": "/nicholas-cerisano-resume/static/js/2.553d87a2.chunk.js"
+  },
+  {
+    "revision": "7ff116c93424d91c22e8",
+    "url": "/nicholas-cerisano-resume/static/js/main.714763ee.chunk.js"
+  },
+  {
+    "revision": "2a4d1d957337ad34e22e",
+    "url": "/nicholas-cerisano-resume/static/js/runtime~main.57d7e6dd.js"
+  }
+]);
