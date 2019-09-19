cd7d4a85e7acc51da47fa3703d4668c8daffbdf7
EpicNicks <epicnicks2.0@gmail.com>
2019-09-19 00:44:01 -0400

    added IE 11 polyfill package

--- src/scripts/pages/project-page/ProjectComponents/Portfolio.tsx
+++ src/scripts/pages/project-page/ProjectComponents/Portfolio.tsx
@@ -11,9 +11,8 @@ export class Portfolio extends Component{
                 </h1>
                 <p>
                     I built this website myself in React using TypeScript.<br/>
-                    You can check out the code on my GitHub using the GitHub icon in the 'Contact Me' bar below, or by clicking
-                    <a href="https://github.com/epicnicks" target="_blank" rel="noopener noreferrer"> here.</a><br/>
-
+                    You can check out the code on my GitHub using the GitHub icon in the 'Contact Me' bar below, or by
+                    clicking <a href="https://github.com/epicnicks" target="_blank" rel="noopener noreferrer">here</a>.<br/>
                 </p>
                 <img
                     //get an image of the finished homepage
