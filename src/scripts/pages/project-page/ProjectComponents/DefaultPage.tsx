152f90dde79fb733aeb026b1e795796544833b31
Nicholas Cerisano <epicnicks2.0@gmail.com>
2025-08-15 15:18:36 -0400

    updated homepage to custom page in package.json

--- src/scripts/pages/project-page/ProjectComponents/DefaultPage.tsx
+++ src/scripts/pages/project-page/ProjectComponents/DefaultPage.tsx
@@ -3,11 +3,13 @@ import React from "react";
 export function DefaultPage() {
   return (
     <div className="ProjectDefault">
-      <h1>Welcome to the Project Page</h1>
-      <h3>
-        Feel free to click on the sidebar buttons to get a look at some of my
-        personal projects.
-      </h3>
+      <div className="ProjectDefaultText">
+        <h1>Welcome to the Project Page</h1>
+        <h3>
+          Feel free to click on the sidebar buttons to get a look at some of my
+          personal projects.
+        </h3>
+      </div>
     </div>
   );
 }
