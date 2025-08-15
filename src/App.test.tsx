7d239815a2e438f20c2666d4fa0355cf9d4bd4a3
Nicholas Cerisano <epicnicks2.0@gmail.com>
2025-08-15 02:34:35 -0400

    updated react version - fixed test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+it("renders without crashing", () => {
+  const container = document.getElementById("root");
+  if (!container) {
+    throw new Error("Root element not found");
+  }
+  const root = createRoot(container);
+  root.render(<App />);
 });
