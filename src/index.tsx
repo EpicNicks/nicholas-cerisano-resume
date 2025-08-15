43f8b50535aac82a4aa1a9735ddcb7b4ef7b80ae
Nicholas Cerisano <epicnicks2.0@gmail.com>
2025-08-15 02:32:08 -0400

    updated react version

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,21 @@
 //Polyfills
-import 'react-app-polyfill/ie11';
-import 'core-js/features/array/find';
-import 'core-js/features/array/includes';
-import 'core-js/features/number/is-nan';
+import "react-app-polyfill/ie11";
+import "core-js/features/array/find";
+import "core-js/features/array/includes";
+import "core-js/features/number/is-nan";
 
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const container = document.getElementById("root");
+if (!container) {
+  throw new Error("Root element not found");
+}
+const root = createRoot(container);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
